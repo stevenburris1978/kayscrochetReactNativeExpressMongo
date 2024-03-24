c4fbe6e59d0502ef2cd14782472089b2f913bbaa
skylor burris <s_burris@mail.fhsu.edu>
2024-03-23 19:55:37 -0500

    kayscrochetmobileapp 1.0

--- kayscrochet-backend/server.js
+++ kayscrochet-backend/server.js
@@ -111,8 +111,8 @@ const uploadToS3 = async (image, key) => {
 
 app.use(helmet());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use((err, req, res, next) => {
     console.error(err.stack);  
