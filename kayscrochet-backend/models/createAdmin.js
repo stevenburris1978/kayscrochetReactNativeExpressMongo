6e1acb8145bc6adf90131d372c21195c379dcc56
skylor burris <s_burris@mail.fhsu.edu>
2024-04-16 18:59:14 -0500

    sample createAdmin.js model, update readme, comments

---
+++ kayscrochet-backend/models/createAdmin.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const adminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+async function createAdminUser(username, password) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const admin = new Admin({
+        username: username,
+        password: hashedPassword
+    });
+
+    await admin.save();
+    console.log(`Admin user ${username} created`);
+}
+
+createAdminUser('user1', 'password1').then(() => {
+    createAdminUser('user2', 'password2').then(() => {
+        mongoose.connection.close();
+    });
+});
